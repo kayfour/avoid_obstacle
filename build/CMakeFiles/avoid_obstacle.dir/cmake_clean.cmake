77ac2b92ee82525ebbf0e265bb8b8c1fbbc862cc
kayfour <kayfour@hanmail.net>
2020-12-02 18:24:37 +0900

    first commit

---
+++ build/CMakeFiles/avoid_obstacle.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/avoid_obstacle.dir/avoid_obstacle.cpp.o"
+  "avoid_obstacle"
+  "avoid_obstacle.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/avoid_obstacle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
